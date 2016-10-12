e33946f581e6d7eaed72bbd54c1571746a362079
Jordan Ide <jorrdandied@hotmail.com>
2016-10-12 14:24:32 +0100

    add basic tests

--- test/index.js
+++ test/index.js
@@ -1,14 +1,21 @@
 import http from 'http';
 import assert from 'assert';
 
-import '../lib/index.js';
-
 //This test doesn't actually work for our case but its here just to show we can test these scripts.
-describe('Example Node Server', () => {
-  it('should return 200', done => {
-    http.get('http://127.0.0.1:1337', res => {
+describe('Node Server', () => {
+
+  it('should be running graphiql', done => {
+    http.get('http://127.0.0.1:4000/graphiql', res => {
       assert.equal(200, res.statusCode);
       done();
     });
   });
+
+  it('should 405 on get request', done => {
+    http.get('http://127.0.0.1:4000/graphql', res => {
+      assert.equal(405, res.statusCode);
+      done();
+    })
+  })
+
 });
